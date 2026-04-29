c6d941ba6bdeac1c6558115b6f7f3aec313bbb58
Terence Auyong <terence.a@electronicscience.net>
2026-04-29 11:28:22 +0800

    removed standalone config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-    experimental: {
+  experimental: {
     workerThreads: false,
     cpus: 1
   }
